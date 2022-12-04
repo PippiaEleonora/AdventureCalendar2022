78100f4257c67a2e8548aa9a38577f463c7c3d39
PippiaEleonora <43842574+PippiaEleonora@users.noreply.github.com>
2022-12-04 11:20:26 +0100

    Day3_2022

---
+++ Day3.py
@@ -0,0 +1,33 @@
+f = open("/uploads/Input4.txt", "r")
+Packs = f.readlines()
+#Packs = Packs[0].split(" ") 
+
+# First part
+dimension = [len(p)-1 for p in Packs]
+Front = [p[0:round(d/2) ] for d, p in zip(dimension,Packs)] 
+Back = [p[round(d/2)::].replace("\n","") for d, p in zip(dimension,Packs)]
+
+Obj = [""]*len(Front)
+for i in range(len(Front)):
+    for f in Front[i]:
+        if f in Back[i]:
+            Obj[i] = f
+                
+            
+Priority = [ord(x)-96 if ord(x)>96 else ord(x) - 64+26 for x in Obj]
+print(sum(Priority))
+
+# Second part
+Elf1 = [Packs[i].replace("\n","")  for i in range(len(Packs))  if i%3==0] 
+Elf2 = [Packs[i].replace("\n","")  for i in range(len(Packs))  if i%3==1] 
+Elf3 = [Packs[i].replace("\n","") for i in range(len(Packs))  if i%3==2] 
+
+Obj = [""]*len(Elf1)
+for i in range(len(Elf1)):
+    for f in Elf1[i]:
+        if f in Elf2[i] and f in Elf3[i]:
+            Obj[i] = f
+                
+            
+Priority = [ord(x)-96 if ord(x)>96 else ord(x) - 64+26 for x in Obj]
+print(sum(Priority))
