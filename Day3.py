47d5f8c3d54dcfece6d757795c2def19bd102e0a
PippiaEleonora <43842574+PippiaEleonora@users.noreply.github.com>
2022-12-05 07:37:57 +0100

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -2,7 +2,7 @@ f = open("/uploads/Input4.txt", "r")
 Packs = f.readlines()
 #Packs = Packs[0].split(" ") 
 
-# First part
+## First part
 dimension = [len(p)-1 for p in Packs]
 Front = [p[0:round(d/2) ] for d, p in zip(dimension,Packs)] 
 Back = [p[round(d/2)::].replace("\n","") for d, p in zip(dimension,Packs)]
@@ -12,12 +12,18 @@ for i in range(len(Front)):
     for f in Front[i]:
         if f in Back[i]:
             Obj[i] = f
-                
-            
 Priority = [ord(x)-96 if ord(x)>96 else ord(x) - 64+26 for x in Obj]
 print(sum(Priority))
 
-# Second part
+# Mode 2                
+Obj = [list(set(x).intersection(set(y)))[0] for x, y in zip(Front, Back)]
+Priority = [ord(x)-96 if ord(x)>96 else ord(x) - 64+26 for x in Obj]
+print(sum(Priority))
+
+            
+
+
+## Second part
 Elf1 = [Packs[i].replace("\n","")  for i in range(len(Packs))  if i%3==0] 
 Elf2 = [Packs[i].replace("\n","")  for i in range(len(Packs))  if i%3==1] 
 Elf3 = [Packs[i].replace("\n","") for i in range(len(Packs))  if i%3==2] 
@@ -31,3 +37,8 @@ for i in range(len(Elf1)):
             
 Priority = [ord(x)-96 if ord(x)>96 else ord(x) - 64+26 for x in Obj]
 print(sum(Priority))
+
+#Mode 2
+Obj = [list(set(x).intersection(set(y).intersection(set(z))))[0] for x, y, z in zip(Elf1, Elf2, Elf3)]
+Priority = [ord(x)-96 if ord(x)>96 else ord(x) - 64+26 for x in Obj]
+print(sum(Priority)) 
