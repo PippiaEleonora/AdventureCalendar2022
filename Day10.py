22c304134295a3b013c2452fd7682718bcabd2e3
PippiaEleonora <43842574+PippiaEleonora@users.noreply.github.com>
2022-12-10 18:20:14 +0100

    Day10 complete

--- Day10.py
+++ Day10.py
@@ -2,16 +2,19 @@ f = open("/uploads/Input10.txt")
 Array = f.readlines()
 
 count = [1]
+curr_tot = [1]
 time = [0]
 time_real = 0
 for line in Array:
     if "noop" in line:
         count.append(0)
+        curr_tot.append(sum(count))
         time.append(time_real + 1)
         time_real += 1
     else:
         line = line.replace("\n","").replace("addx ", "")
-        count.append(int(line))
+        count. append(int(line)) 
+        curr_tot.append(sum(count)) 
         time.append(time_real + 2)
         time_real += 2
         
@@ -28,3 +31,16 @@ for t in range(6):
 
 
 print(TOT) 
+
+#Second part
+Board = [['.']*40, ['.']*40, ['.']*40, ['.']*40, ['.']*40, ['.']*40]
+t = 0
+for i in range(240):
+    if i == time[t+1]:
+        t += 1
+    if abs(curr_tot[t]-(i%40)) < 2:
+        Board[i//40][i%40] = '#'
+        
+
+Board2 = ["".join(k) for k in Board]
+print("\n".join(Board2))
